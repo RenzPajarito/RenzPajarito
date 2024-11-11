4b4faea146fa2155ac9814630d33888f1d05e0c3
Renz Pajarito <renzpajarito08@gmail.com>
2024-11-11 20:13:56 +0800

    web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased ${poppins.className}`}>{children}</body>
+      <body className={`antialiased ${poppins.className}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
